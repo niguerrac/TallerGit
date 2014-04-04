c3ab94e7623de1ac03a5f44bc6e700fe78c32863
Nilson Guerra <niguerra@alumnos.ubiobio.cl>
2014-04-04 11:15:13 -0400

    2

--- TallerBasicoGit/src/tallerbasicogit/TallerBasicoGit.java
+++ TallerBasicoGit/src/tallerbasicogit/TallerBasicoGit.java
@@ -16,7 +16,7 @@ public class TallerBasicoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logi
         System.out.println("Printer comit");
     }
     
